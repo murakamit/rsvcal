3cf6fc689dbd862bd2bb79ff12dcbd137ec86cc7
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-07-06 06:35:02 +0900

    initial add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rsvcal::Application.config.secret_key_base = '817727eb04156605d1faad7fe760ceade1fffabddf024607e26f5a3a8f809c504e87a15476bd39eea63ab9a835392051da1a842bdd5a5cf0de3684ac5f841b1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rsvcal::Application.config.session_store :cookie_store, key: '_rsvcal_session'
