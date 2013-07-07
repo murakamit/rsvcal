875546f27b388e09006394228806dcb8be022f4b
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-07-07 19:07:23 +0900

    pass userable

--- spec/models/weekly_spec.rb
+++ spec/models/weekly_spec.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 
 describe Weekly do
-  context "user.size" do
+  describe "user" do
     let(:g) { Group.create! name: "g" }
     let(:i) { Item.create! group: g, name: "i" }
     let(:h) {
@@ -13,23 +13,41 @@ describe Weekly do
         end:   "2010-01-01 10:00",
       }
     }
-    let(:a0) { "a"  * 50 }
-    let(:j0) { "あ" * 50 }
-    let(:a1) { "a"  * 51 }
-    let(:j1) { "あ" * 51 }
-    it {
-      h[:user] = a0
-      w = nil
-      expect { w = Weekly.create! h }.not_to raise_error
-      expect { w.update! user: a1 }.to raise_error
-    }
-    it { expect { Weekly.create! user: a1 }.to raise_error }
-    it {
-      h[:user] = j0
-      w = nil
-      expect { w = Weekly.create! h }.not_to raise_error
-      expect { w.update! user: j1 }.to raise_error
-    }
-    it { expect { Weekly.create! user: j1 }.to raise_error }
-  end
+
+    context "empty, blank" do
+      it { expect { w = Weekly.create! h }.to raise_error }
+
+      [ nil, '', " ", "　", "\n", "\r\n", "\n \n" ].each { |s|
+        it {
+          h[:user] = s
+          expect { w = Weekly.create! h }.to raise_error
+        }
+      }
+    end
+
+    context "size" do
+      let(:a0) { "a"  * 50 }
+      let(:j0) { "あ" * 50 }
+      let(:a1) { "a"  * 51 }
+      let(:j1) { "あ" * 51 }
+
+      it {
+        h[:user] = a0
+        w = nil
+        expect { w = Weekly.create! h }.not_to raise_error
+        expect { w.update! user: a1 }.to raise_error
+      }
+
+      it { expect { Weekly.create! user: a1 }.to raise_error }
+
+      it {
+        h[:user] = j0
+        w = nil
+        expect { w = Weekly.create! h }.not_to raise_error
+        expect { w.update! user: j1 }.to raise_error
+      }
+
+      it { expect { Weekly.create! user: j1 }.to raise_error }
+    end
+  end # "user"
 end
