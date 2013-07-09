481e051257205818ebe5bc3e3d508a504469c6bd
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-07-09 18:05:05 +0900

    generate model reservation

---
+++ db/migrate/20130709090044_create_reservations.rb
@@ -0,0 +1,18 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.integer  :item_id, :null => false
+      t.string   :user,    :null => false, :limit =>  50
+      t.date     :date,    :null => false
+      t.integer  :begin_h, :null => false
+      t.integer  :begin_m, :null => false
+      t.integer  :end_h,   :null => false
+      t.integer  :end_m,   :null => false
+      t.string   :icon,    :null => false, :limit =>  50, :default => '&#9734;'
+      t.string   :memo,    :null => false, :limit => 250, :default => ''
+      t.datetime :removed_at,   :null => false, :default => '1900-01-01'
+      t.integer  :lock_version, :null => false, :default => 0
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130708223846) do
+ActiveRecord::Schema.define(version: 20130709090044) do
 
   create_table "groups", force: true do |t|
     t.string   "name",         limit: 50,                                  null: false
@@ -25,6 +25,22 @@ ActiveRecord::Schema.define(version: 20130708223846) do
   create_table "items", force: true do |t|
     t.integer  "group_id",                                                 null: false
     t.string   "name",         limit: 50,                                  null: false
+    t.string   "memo",         limit: 250, default: "",                    null: false
+    t.datetime "removed_at",               default: '1900-01-01 00:00:00', null: false
+    t.integer  "lock_version",             default: 0,                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reservations", force: true do |t|
+    t.integer  "item_id",                                                  null: false
+    t.string   "user",         limit: 50,                                  null: false
+    t.date     "date",                                                     null: false
+    t.integer  "begin_h",                                                  null: false
+    t.integer  "begin_m",                                                  null: false
+    t.integer  "end_h",                                                    null: false
+    t.integer  "end_m",                                                    null: false
+    t.string   "icon",         limit: 50,  default: "&#9734;",             null: false
     t.string   "memo",         limit: 250, default: "",                    null: false
     t.datetime "removed_at",               default: '1900-01-01 00:00:00', null: false
     t.integer  "lock_version",             default: 0,                     null: false
