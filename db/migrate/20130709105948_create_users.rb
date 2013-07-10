61ae25e50dffc3d253b154f4bfaa955c3dbec0ef
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-07-10 21:24:21 +0900

    user@db uniq: true

--- db/migrate/20130709105948_create_users.rb
+++ db/migrate/20130709105948_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string   :name,         :null => false, :limit =>  50
+      t.string   :name,         :null => false, :limit =>  50, :unique => true
       t.string   :password_digest, :null => false
       t.string   :memo,         :null => false, :limit => 250, :default => ''
       t.boolean  :admin,        :null => false, :default => false
