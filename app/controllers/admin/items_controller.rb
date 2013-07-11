b176cfc2953d03a7bab1fcc924f66907811e9579
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-07-12 07:02:06 +0900

    admin::items#edit, #update, #destroy

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -18,11 +18,32 @@ class Admin::ItemsController < Admin::Base
   end
 
   def edit
+    @item = Item.find params[:id]
+    @page_title = "#{@item.name}"
+    @errors = flash[:errors]
+  rescue
+    redirect_to items_path
   end
 
   def update
+    item = Item.find params[:id]
+    if item.update params.require(:item).permit(:group_id, :name, :memo)
+      redirect_to item, notice: "updated."
+    else
+      e = item.errors
+      flash[:errors] = e
+      n = e.size
+      redirect_to [:edit, :admin, item], alert: "#{n} error#{'s' if n > 1}"
+    end
+  rescue
+    redirect_to items_path
   end
 
   def destroy
+    item = Item.find params[:id]    
+    item.remove
+    redirect_to items_path, notice: "deleted."
+  rescue
+    redirect_to items_path
   end
 end
