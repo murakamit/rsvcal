4ece6409742dc37d57307c3d1f015e5e1170d7e4
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-07-13 23:41:23 +0900

    select list for item@grooup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def select_items_tag(form, key, selected = nil)
+    groups = Group.order(:name)
+    return "" if groups.empty?
+    h = Hash.new { |h_, k_| h_[k_] = [] }
+    groups.each { |g|
+      g.items.order(:name).each { |item| 
+        h[g.name] << [item.name, item.id]
+      }
+    }
+    form.select key, grouped_options_for_select(h, selected: selected)
+  end
 end
